2495ee9a7afbb642f656feed70a45379082d7008
liseth <sandovaliseth24@gmail.com>
2022-04-21 19:55:08 -0500

    segundo avance

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const btnswitch = document.querySelector('#switch');
+
+btnswitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active');
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+if(localStorage.getItem('dark-mode') == 'true'){
+    document.body.classList.add('dark');
+    btnswitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    btnswitch.classList.remove('active');
+}
